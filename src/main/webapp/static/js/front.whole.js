c4cbf109b0c83699b44b074ee196bac0aa26c71c
wanghongmeng <wanghongmeng@live.cn>
2013-11-13 14:32:08 +0800

    修改scrolltopjs

--- src/main/webapp/static/js/front.whole.js
+++ src/main/webapp/static/js/front.whole.js
@@ -1,5 +1,5 @@
 var duoshuoQuery = {short_name:"wanghongmeng"};
-(function() {
+var duoshuo = (function(){
     var ds = document.createElement('script');
     ds.type = 'text/javascript';ds.async = true;
     ds.src = 'http://static.duoshuo.com/embed.js';
@@ -63,21 +63,22 @@ var front = (function(){
             $(".menu .ca-menu li a .ca-icon").css("line-height", newHeight + "px");
         },
         goTop : function(){
-            var obj=document.getElementById("gotop");
             function getScrollTop(){
-                return document.documentElement.scrollTop;
+                return $(window).scrollTop();
             }
             function setScrollTop(value){
-                document.documentElement.scrollTop=value;
+                $(window).scrollTop(value);
             }
-            window.onscroll=function(){getScrollTop()>0?obj.style.display="":obj.style.display="none";}
-            obj.onclick=function(){
-                var goTop=setInterval(scrollMove,10);
+            $(window).scroll(function(){
+                getScrollTop() > 0 ? $("#gotop").css("display","") : $("#gotop").css("display","none");
+            });
+            $("#gotop").bind("click",function(){
+                var gotop = setInterval(scrollMove,10);
                 function scrollMove(){
                     setScrollTop(getScrollTop()/1.1);
-                    if(getScrollTop()<1)clearInterval(goTop);
+                    if(getScrollTop()<1)clearInterval(gotop);
                 }
-            }
+            });
         }
     };
 })();
